ef1ff1f99ec206e02f86c4bb7fc5d468c77cfd7a
nandweza <allannandweza@gmail.com>
2023-04-17 07:59:16 +0300

    connected database, created home, login and register routes

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,1 +1,80 @@
 //jshint esversion:6
+const express = require('express');
+const app = express();
+
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongooseEncryption = require('mongoose-encryption');
+const User = require('./models/user');
+
+
+const port = 3000 || process.env.PORT;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB',
+{ useNewUrlParser: true })
+.then(() => {
+    console.log('DB Connected successfully');
+}).catch((error) => {
+    console.log(error);
+});
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+//login routes
+
+app.route('/login')
+.get((req, res) => {
+    res.render('login');
+})
+
+.post(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+        if (user.password === password) {
+            res.render('secrets');
+        }
+    } else {
+        console.log('Not authorized');
+    }
+})
+
+//register routes
+
+app.route('/register')
+
+.get((req, res) => {
+    res.render('register');
+})
+
+.post((req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User({ email, password });
+
+    newUser.save()
+    .then(() => {
+        res.render('secrets');
+    }).catch((error) => {
+        console.log(error);
+    });
+})
+
+
+
+app.listen(port, () => {
+    console.log(`App is listening on port: ${port}`);
+})
